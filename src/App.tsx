53bca4b02407d14883987314ebed4323608a1233
JandsonTalison <jandsontalison007@gmail.com>
2022-03-22 01:52:51 -0300

    finalizando versao 1.0 todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item }  from './types/Item';
+import { ListItem } from './Components/ListItem';
+import { AddArea } from './Components/AddArea';
 // import {Container} from './App.styles';
 const App = () => {
+
+  const [list,setList] = useState<Item[]>([
+    // {id: 1,name:'comprar pão',done: false},
+    // {id: 2,name:'malhar',done: true}
+  ]);
+
+  const handleAddTask = (taskName: string) =>{
+    let newList = [...list];
+    newList.push({
+      id: list.length+1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
   return(
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+        {<AddArea onEnter={handleAddTask}/>}
+        {list.map((item,index)=>(
+          <ListItem key={index} item = {item}/>
+        ))}
       </C.Area>
     </C.Container>
   );
